3179875532f4d2cdb5c2d78901904362eca08845
Joselito <eu@joseli.to>
2021-09-04 02:33:12 -0300

    adds newsletter feed

--- pt/scripts/cleaner.js
+++ pt/scripts/cleaner.js
@@ -3,22 +3,34 @@ const CleanCSS = require('clean-css');
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, '..', 'output') + '/wiki.html', 'utf8', async (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
+const Parser = require('rss-parser');
+const parser = new Parser();
 
-  const root = HTMLParser.parse(data);
+const newsletterFeedItems = [];
 
-  root.querySelector('meta[name="copyright"]').remove();
-  root.querySelector('meta[name="viewport"]').remove();
-  root.querySelector('meta[content="IE=Edge"]').remove();
-  root.querySelector('meta[content*="text/html"]').remove();
-  root.querySelector('#faviconLink').remove();
-  root.querySelector('title').remove();
+(async () => {
 
-  root.querySelector('head').insertAdjacentHTML('beforeend', `
+  const newsletterFeed = await parser.parseURL('https://world.hey.com/joselito/feed.atom');
+  newsletterFeed.items.forEach(item => {
+    newsletterFeedItems.push('* [[' + item.title + '|' + item.link + ']]')
+  });
+
+  fs.readFile(path.join(__dirname, '..', 'output') + '/wiki.html', 'utf8', async (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    const root = HTMLParser.parse(data);
+
+    root.querySelector('meta[name="copyright"]').remove();
+    root.querySelector('meta[name="viewport"]').remove();
+    root.querySelector('meta[content="IE=Edge"]').remove();
+    root.querySelector('meta[content*="text/html"]').remove();
+    root.querySelector('#faviconLink').remove();
+    root.querySelector('title').remove();
+
+    root.querySelector('head').insertAdjacentHTML('beforeend', `
     <meta charset="utf-8" />
     <meta content="width=device-width,initial-scale=1" name="viewport">
     <meta name="theme-color" content="#653585" />
@@ -51,55 +63,60 @@ fs.readFile(path.join(__dirname, '..', 'output') + '/wiki.html', 'utf8', async (
     <title>Joseli.to — Uma página pessoal</title>
   `);
 
-  root.querySelector('noscript').remove();
-  root.querySelector('#styleArea').remove();
-  root.querySelector('div[author="Flibbles"]').remove();
-  root.querySelector('div[author="Scott Kingery"]').remove();
+    root.querySelector('noscript').remove();
+    root.querySelector('#styleArea').remove();
+    root.querySelector('div[author="Flibbles"]').remove();
+    root.querySelector('div[author="Scott Kingery"]').remove();
 
-  let _a1 = root.querySelector('div[name="Core"] pre').innerHTML.toString().indexOf("&quot;$:/palettes/Blanca&quot;");
-  let _a2 = root.querySelector('div[name="Core"] pre').innerHTML.toString().indexOf("&quot;$:/palettes/Vanilla&quot;:{&quot;name&quot;:&quot;Vanilla");
+    let _a1 = root.querySelector('div[name="Core"] pre').innerHTML.toString().indexOf("&quot;$:/palettes/Blanca&quot;");
+    let _a2 = root.querySelector('div[name="Core"] pre').innerHTML.toString().indexOf("&quot;$:/palettes/Vanilla&quot;:{&quot;name&quot;:&quot;Vanilla");
 
-  let _txt1 = root.querySelector('div[name="Core"] pre').innerHTML.substring(0, _a1)
-  let _txt2 = root.querySelector('div[name="Core"] pre').innerHTML.substring(_a2, root.querySelector('div[name="Core"] pre').innerHTML.length);
+    let _txt1 = root.querySelector('div[name="Core"] pre').innerHTML.substring(0, _a1)
+    let _txt2 = root.querySelector('div[name="Core"] pre').innerHTML.substring(_a2, root.querySelector('div[name="Core"] pre').innerHTML.length);
 
-  let _a = _txt1 + _txt2;
+    let _a = _txt1 + _txt2;
 
-  root.querySelector('div[name="Core"] pre').set_content(_a);
+    root.querySelector('div[name="Core"] pre').set_content(_a);
 
-  _a1 = root.querySelector('div[name="Vanilla"] pre').innerHTML.toString().indexOf("/*! modern-normalize v1.0.0");
-  _a2 = root.querySelector('div[name="Vanilla"] pre').innerHTML.toString().indexOf("Add the correct display in Chrome and Safari.");
+    _a1 = root.querySelector('div[name="Vanilla"] pre').innerHTML.toString().indexOf("/*! modern-normalize v1.0.0");
+    _a2 = root.querySelector('div[name="Vanilla"] pre').innerHTML.toString().indexOf("Add the correct display in Chrome and Safari.");
 
-  _txt1 = root.querySelector('div[name="Vanilla"] pre').innerHTML.substring(0, _a1)
-  _txt2 = root.querySelector('div[name="Vanilla"] pre').innerHTML.substring(_a2 + 90, root.querySelector('div[name="Vanilla"] pre').innerHTML.length);
+    _txt1 = root.querySelector('div[name="Vanilla"] pre').innerHTML.substring(0, _a1)
+    _txt2 = root.querySelector('div[name="Vanilla"] pre').innerHTML.substring(_a2 + 90, root.querySelector('div[name="Vanilla"] pre').innerHTML.length);
 
-  _a = _txt1 + _txt2;
+    _a = _txt1 + _txt2;
 
-  root.querySelector('div[name="Vanilla"] pre').set_content(_a);
+    root.querySelector('div[name="Vanilla"] pre').set_content(_a);
 
-  root.querySelector('div[title="$:/estilo/Global"] pre').replaceWith(`
+    const content = root.querySelector('div[title="Conteúdo"] pre');
+
+    content.replaceWith("<pre>" + content.innerHTML.replace('!! [[Newsletter]] !!', `!! [[Newsletter]]\n${newsletterFeedItems.join('\n')}`) + "</pre>");
+
+    root.querySelector('div[title="$:/estilo/Global"] pre').replaceWith(`
     <pre>${new CleanCSS({})
-      .minify(root.querySelector('div[title="$:/estilo/Global"] pre')
-        .innerHTML
-        .replaceAll('&quot;', '"')
-        .replaceAll('&gt;', '>')
-      ).styles
-      .replaceAll('"', '&quot;')
-      .replaceAll('>', '&gt;')}
+        .minify(root.querySelector('div[title="$:/estilo/Global"] pre')
+          .innerHTML
+          .replaceAll('&quot;', '"')
+          .replaceAll('&gt;', '>')
+        ).styles
+        .replaceAll('"', '&quot;')
+        .replaceAll('>', '&gt;')}
     </pre>`)
 
-  root.querySelector('div[title="$:/estilo/HomePage"] pre').replaceWith(`
+    root.querySelector('div[title="$:/estilo/HomePage"] pre').replaceWith(`
     <pre>${new CleanCSS({})
-      .minify(root.querySelector('div[title="$:/estilo/HomePage"] pre')
-        .innerHTML
-        .replaceAll('&quot;', '"')
-        .replaceAll('&gt;', '>')
-      ).styles
-      .replaceAll('"', '&quot;')
-      .replaceAll('>', '&gt;')}
+        .minify(root.querySelector('div[title="$:/estilo/HomePage"] pre')
+          .innerHTML
+          .replaceAll('&quot;', '"')
+          .replaceAll('&gt;', '>')
+        ).styles
+        .replaceAll('"', '&quot;')
+        .replaceAll('>', '&gt;')}
     </pre>`)
 
-  fs.writeFile(path.join(__dirname, '..', 'output') + '/index.min.html', root.toString(), function (err) {
-    if (err) return console.log(err);
-    // console.log('Hello World > helloworld.txt');
-  });
-})
+    fs.writeFile(path.join(__dirname, '..', 'output') + '/index.min.html', root.toString(), function (err) {
+      if (err) return console.log(err);
+      // console.log('Hello World > helloworld.txt');
+    });
+  })
+})();
---
+++ pt/tiddlers/$__macros_breakzplatform_lastfm.js
@@ -0,0 +1,47 @@
+/*\
+title: $:/macros/breakzplatform/lastfm.js
+type: application/javascript
+module-type: macro
+
+<<lastfm user>>
+
+Example:
+<<lastfm>>
+<<lastfm "Donald Duck">>
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Information about this macro
+This is the mysamplemacro of Tiddly Wiki 5 written in JavaScript 
+*/
+
+exports.name = "lastfm";
+
+exports.params = [
+	{ name: "user" }
+];
+
+/*
+Run the macro
+*/
+exports.run = function(user) {
+  const url = 'https://lastfm-last-played.biancarosa.com.br/' + user + '/latest-song';
+	const song = document.querySelector('#song');
+	
+        fetch(url)
+            .then(function (response) {
+                return response.json()
+            }).then(function (json) {
+                song.innerHTML = json['track']['name'] + ' - ' + json['track']['artist']['#text'];
+            }).catch(function (e) {
+								song.innerHTML = ['Hook - Blues Traveler', 'One More Time - Daft Punk'][~~(Math.random() * 2)];;
+						});
+};
+
+})();
