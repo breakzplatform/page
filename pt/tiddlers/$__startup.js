98443e138e91cdcdebda697a5e6acff701eda3de
Joselito <eu@joseli.to>
2021-09-04 18:00:34 -0300

    sitemap and input search aria-label

---
+++ pt/tiddlers/$__startup.js
@@ -0,0 +1,23 @@
+/*\
+title: $:/startup.js
+type: application/javascript
+module-type: startup
+
+Startup
+
+\*/
+(function () {
+
+	/*jslint node: true, browser: true */
+	/*global $tw: false */
+	"use strict";
+
+	exports.name = "startup";
+	exports.platforms = ["browser"];
+	exports.after = ["render"];
+	exports.synchronous = true;
+
+	exports.startup = function() {
+		document.querySelector('input[type="search"]').setAttribute("aria-label", "Buscar no site");
+	}
+})();
