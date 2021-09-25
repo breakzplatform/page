8f82c86313c7a914a43a22e8d46c5a5096c2f9b9
Joselito <eu@joseli.to>
2021-09-25 18:10:23 -0300

    typo

--- pt/scripts/cleaner.js
+++ pt/scripts/cleaner.js
@@ -171,7 +171,7 @@ const newsletterFeedItems = [];
     </pre>`)
 
     // VLibras
-    root.querySelector('body').insertAdjacentHTML('beforeend', `<div vw class="enabled"><div vw-access-button class="active"></div><div vw-plugin-wrapper><div class="vw-plugin-top-wrapper"></div></div></div><script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script><script>new window.VLibras.Widget('https://vlibras.gov.br/app');</script><style>[vw] [vw-access-button] .access-button {important;border-radius:100%}</style>`);
+    root.querySelector('body').insertAdjacentHTML('beforeend', `<div vw class="enabled"><div vw-access-button class="active"></div><div vw-plugin-wrapper><div class="vw-plugin-top-wrapper"></div></div></div><script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script><script>new window.VLibras.Widget('https://vlibras.gov.br/app');</script><style>[vw] [vw-access-button] .access-button {border-radius:100%}</style>`);
 
     fs.writeFile(path.join(__dirname, '..', 'output') + '/wiki.min.html', root.toString(), function (err) {
       if (err) return console.log(err);
