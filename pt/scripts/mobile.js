bea6c6d0a8a242506a03619573d5aefaedd0d49a
Joselito <eu@joseli.to>
2021-09-27 02:25:08 -0300

    mobile optimization cleanup

--- pt/scripts/mobile.js
+++ pt/scripts/mobile.js
@@ -12,6 +12,9 @@ const path = require('path');
     const root = HTMLParser.parse(data);
 
     root.querySelector('div[title="$:/plugins/rmnvsl/krystal"]').remove();
+    root.querySelector('div[title="$:/plugins/rmnvsl/krystal/plugin.js"]').remove();
+    root.querySelector('div[title="$:/plugins/rmnvsl/krystal/headertools"]').remove();
+    root.querySelector('div[title="$:/plugins/rmnvsl/krystal/images/maximize-tiddler"]').remove();
     root.querySelector('div[title="$:/SiteTitle"] pre').set_content('[img width=128 height=128 class=&quot;eu&quot; alt=&quot;Avatar Joselito&quot; [https://next.joseli.to/pt/icon.webp]]<span style="display:none">Joseli.to</span>');
     root.querySelector('head').insertAdjacentHTML('beforeend', `<style>.tc-sidebar-header,.tc-sidebar-lists .tc-tab-buttons {text-align: center;}.tc-topbar{display: none;}.tc-sidebar-tabs{text-align: left;}.tc-tab-content {font-size:1rem;}.tc-tab-content ul {list-style:none;margin:0;padding: 0;}.tc-site-title .eu {margin: 0 0 10px}.tc-tiddler-frame{border-radius: 4px;box-shadow:0 0 8px 0 rgb(0 0 0 / 10%)}</style>`);
 
