0835d77288183485489165d104981a76c0bef1b2
Joselito <eu@joseli.to>
2021-10-03 17:26:58 -0300

    upgrde to tiddlywiki 5.2.0

--- pt/scripts/mobile.js
+++ pt/scripts/mobile.js
@@ -11,11 +11,21 @@ const path = require('path');
 
     const root = HTMLParser.parse(data);
 
-    root.querySelector('div[title="$:/plugins/rmnvsl/krystal"]').remove();
-    root.querySelector('div[title="$:/plugins/rmnvsl/krystal/plugin.js"]').remove();
-    root.querySelector('div[title="$:/plugins/rmnvsl/krystal/headertools"]').remove();
-    root.querySelector('div[title="$:/plugins/rmnvsl/krystal/images/maximize-tiddler"]').remove();
-    root.querySelector('div[title="$:/SiteTitle"] pre').set_content('[img width=128 height=128 class=&quot;eu&quot; alt=&quot;Avatar Joselito&quot; [https://next.joseli.to/pt/icon.webp]]<span style="display:none">Joseli.to</span>');
+    const jsonContent = root.querySelector('.tiddlywiki-tiddler-store').innerHTML;
+    const jsonContentMod = [];
+
+    jsonContent.split('\n').forEach(line => {
+      if (line.includes('"title":"$:/plugins/rmnvsl/krystal"') || line.includes('"title":"$:/plugins/rmnvsl/krystal/plugin.js"') || line.includes('"title":"$:/plugins/rmnvsl/krystal/headertools"') || line.includes('"title":"$:/plugins/rmnvsl/krystal/images/maximize-tiddler"')) {
+        // removes unecessary tiddlers
+        console.log('Removed');
+      } else if (line.includes('"title":"$:/SiteTitle"')) {
+        jsonContentMod.push('{"title":"$:/SiteTitle","created":"20210828175705185","modified":"20210828232243746","type":"text/vnd.tiddlywiki","text":"\\u003Cimg loading=\\"lazy\\" src=\\"https://next.joseli.to/pt/icon.webp\\" class=\\"eu\\" width=\\"128\\" height=\\"128\\" alt=\\"Avatar Joselito\\">\\u003Cspan style=\\"display:none\\">Joseli.to\\u003C/span>","revision":"0","bag":"default"},')
+      } else {
+        jsonContentMod.push(line);
+      }
+    });
+
+    root.querySelector('.tiddlywiki-tiddler-store').set_content(jsonContentMod.join(''));
     root.querySelector('head').insertAdjacentHTML('beforeend', `<style>.tc-sidebar-header,.tc-sidebar-lists .tc-tab-buttons {text-align: center;}.tc-topbar{display: none;}.tc-sidebar-tabs{text-align: left;}.tc-tab-content {font-size:1rem;}.tc-tab-content ul {list-style:none;margin:0;padding: 0;}.tc-site-title .eu {margin: 0 0 10px}.tc-tiddler-frame{border-radius: 4px;box-shadow:0 0 8px 0 rgb(0 0 0 / 10%)}</style>`);
 
     fs.writeFile(path.join(__dirname, '..', 'output') + '/wiki.mobile.min.html', root.toString(), function (err) {
